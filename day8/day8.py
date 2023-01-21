09c7870ee184f4cfb4b57bef6075e390ce16d8e7
ROG\hamid <muhammad.hamid44@gmail.com>
2023-01-21 03:04:51 +0100

    day 8 part 1

--- day8/day8.py
+++ day8/day8.py
@@ -3,6 +3,22 @@
 with open('input.txt', 'r') as file:
     data = file.read().strip().split('\n')
 
-# 
-for line in data:
-    print(line)
+# part 1
+totalVisibleTrees = 0
+rows = len(data)
+columns = len(data[0])
+edges = rows * 2 + columns * 2 - 4
+totalVisibleTrees = edges
+
+for row in range(1, rows - 1):
+  for col in range(1, columns - 1):
+    tree = data[row][col]
+    left = [data[row][col - i] for i in range(1, col + 1)]
+    right = [data[row][col + i] for i in range(1, columns - col)]
+    up = [data[row - i][col] for i in range(1, row + 1)]
+    down = [data[row + i][col] for i in range(1, rows - row)]
+
+    if (max(left) < tree or max(right) < tree or max(up) < tree or max(down) < tree):
+      totalVisibleTrees += 1
+
+print('Total number of visible trees: ', totalVisibleTrees)
